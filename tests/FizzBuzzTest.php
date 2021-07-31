0faa5d50a17a991f384e11ac2db8337a25b2d5d4
Barney Laurance <barney@redmagic.org.uk>
2021-07-31 01:46:22 +0100

    Comment out some test cases to demonstrate mutation coverage failing the build

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -21,20 +21,20 @@ class FizzBuzzTest extends TestCase
     {
         // answers copied from https://en.wikipedia.org/wiki/Fizz_buzz
         return [
-            [1,  '1'        ],
-            [2,  '2'        ],
-            [3,  'Fizz'     ],
-            [4,  '4'        ],
+//            [1,  '1'        ],
+//            [2,  '2'        ],
+//            [3,  'Fizz'     ],
+//            [4,  '4'        ],
             [5,  'Buzz'     ],
-            [6,  'Fizz'     ],
-            [7,  '7'        ],
-            [8,  '8'        ],
-            [9,  'Fizz'     ],
+//            [6,  'Fizz'     ],
+//            [7,  '7'        ],
+//            [8,  '8'        ],
+//            [9,  'Fizz'     ],
             [10, 'Buzz'     ],
-            [11, '11'       ],
-            [12, 'Fizz'     ],
-            [13, '13'       ],
-            [14, '14'       ],
+//            [11, '11'       ],
+//            [12, 'Fizz'     ],
+//            [13, '13'       ],
+//            [14, '14'       ],
             [15, 'Fizz Buzz'],
         ];
     }
